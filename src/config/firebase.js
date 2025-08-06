19fb1816a2408759382c59f0283b700f792f7190
Amarjeet <amarjeetmistri41@gmail.com>
2025-08-06 13:00:25 +0530

    first commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,14 +6,14 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQdKXDDooP_XGk-7KxFfOSCMzP8c4aqWY",
-  authDomain: "real-time-chat-applicati-85034.firebaseapp.com",
-  projectId: "real-time-chat-applicati-85034",
-  storageBucket: "real-time-chat-applicati-85034.firebasestorage.app",
-  messagingSenderId: "358757766407",
-  appId: "1:358757766407:web:f304ad0cfb7fba8d5c21a2",
-  measurementId: "G-QH6688MFXV",
-  databaseURL: "https://real-time-chat-applicati-85034-default-rtdb.firebaseio.com/"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 };
 
 // Initialize Firebase
